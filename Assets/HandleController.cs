71bccc720243a55e4adb48b8f91aba63658cb10c
AlbertNguyen <ndttam.dev@gmail.com>
2019-02-19 16:41:25 +0700

    Done change skybox

---
+++ Assets/HandleController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+
+public class HandleController : MonoBehaviour {
+
+    public Material skybox1;
+    public Material skybox2;
+
+    public GameObject btn1;
+    public GameObject btn2;
+
+    public GameObject mainCamera;
+    float m_FieldOfView;
+
+    bool isSkybox1;
+
+    private void Start()
+    {
+        // Set default FOV
+        m_FieldOfView = 60f;
+    }
+    // Update is called once per frame
+    void Update () {
+        mainCamera.GetComponent<Camera>().fieldOfView = m_FieldOfView;
+    }
+    public void changeScene(){
+        if(isSkybox1){
+            RenderSettings.skybox = skybox2;
+        }else{
+            RenderSettings.skybox = skybox1;
+        }
+        isSkybox1 = !isSkybox1;
+       
+    }
+
+}
