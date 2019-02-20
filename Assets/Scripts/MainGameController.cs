9e51c9c3b1ffac290d5498ae5e018263088bd844
AlbertNguyen <ndttam.dev@gmail.com>
2019-02-20 22:46:25 +0700

    Move smoothy

---
+++ Assets/Scripts/MainGameController.cs
@@ -0,0 +1,148 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainGameController : MonoBehaviour
+{
+    public GameObject BackgroundEnviroment;
+    public GameObject VRPlayerController;
+    //Texture list
+    public Material[] env_material_list;
+
+    public GameObject canvas1;
+    public GameObject canvas2;
+
+    // Transforms to act as start and end markers for the journey.
+    public Transform startMarker;
+    public Transform[] endMarkerList;
+
+    // Movement speed in units/sec.
+    public float speed;
+
+    // Time when the movement started.
+    private float startTime;
+    private float fracJourney;
+    // Total distance between the markers.
+    private float journeyLength = 1;
+
+    private bool isCanRun = false;
+
+    private int indexItemInEndList;
+
+    private Vector3 cameraFirstPosition;
+
+    private int playerMode;
+
+    private bool isStopTime =false;
+
+    // Use this for initialization
+    void Start()
+    {
+        cameraFirstPosition = startMarker.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isCanRun)
+        {
+            // Distance moved = time * speed.
+            float distCovered = (Time.time - startTime) * speed;
+
+            Debug.Log("distCovered: " + distCovered);
+
+            // Fraction of journey completed = current distance divided by total distance.
+            fracJourney = distCovered / journeyLength;
+
+            Debug.Log("Fraction: " + fracJourney);
+
+            // Set position with custom Z
+            Vector3 endMarkerPoint = new Vector3(endMarkerList[indexItemInEndList].position.x, endMarkerList[indexItemInEndList].position.y, endMarkerList[indexItemInEndList].position.z);
+
+            // Set our position as a fraction of the distance between the markers.
+            VRPlayerController.gameObject.transform.position = Vector3.Lerp(cameraFirstPosition, endMarkerPoint, fracJourney);
+
+            // Check distance between marker
+            float distanceStartEnd = Vector3.Distance(startMarker.position, endMarkerPoint);
+
+            Debug.Log("distanceStartEnd: " + distanceStartEnd);
+           
+        }
+
+        if(fracJourney == 1)
+        {
+            StartCoroutine(handleAfterZoom());
+        }
+
+
+ 
+
+    }
+
+    public void  handlePressedButton(int mode)
+    {
+        playerMode = mode;
+        // Move Camera to Postion 
+        makeZoom(mode);
+        //StartCoroutine(handleAfterZoom());  
+    }
+
+
+    IEnumerator handleAfterZoom()
+    {
+        // Delay to wait zoom process finish
+        //yield return new WaitForSeconds(0.5f);
+        // Loading screen fade
+        // Adding Fade script
+        var addingGameObject = gameObject.AddComponent<OVRScreenFade>();
+        // Change Sphere Texture
+        BackgroundEnviroment.GetComponent<Renderer>().sharedMaterial = env_material_list[playerMode - 1];
+        isCanRun = false;
+        previousFirstCameraPosition();
+        yield return new WaitForSeconds(addingGameObject.fadeTime);
+        // Remove Component
+        Destroy(addingGameObject);
+
+    }
+
+    private void makeZoom(int index)
+    {
+        // Keep a note of the time the movement started.
+        startTime = Time.time;
+        // Calculate the journey length.
+        journeyLength = Vector3.Distance(startMarker.position, endMarkerList[index-1].position);
+
+        Debug.Log("Journey Legth: " + journeyLength);
+
+        indexItemInEndList = index - 1;
+        isCanRun = true;
+
+    }
+
+    private void previousFirstCameraPosition(){
+        startMarker.transform.position = Vector3.zero;
+    }
+
+#if UNITY_EDITOR
+    /*
+     * Variables for testing 
+    */
+
+    public int test_mode = 1;
+
+    private void  OnGUI()
+    {
+        if (GUI.Button(new Rect(20, 20, 50, 50), "Press to chage"))
+        {
+            // Change texture testing 
+            BackgroundEnviroment.GetComponent<Renderer>().sharedMaterial = env_material_list[test_mode-1];
+        }
+
+        if (GUI.Button(new Rect(20, 70, 100, 50), "Move Camera"))
+        {
+            handlePressedButton(test_mode);
+        }
+    }
+
+#endif
+}
