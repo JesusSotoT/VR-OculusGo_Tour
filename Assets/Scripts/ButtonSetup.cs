ad129a75965ad632eda287d3d0aae40a736ecb37
AlbertNguyen <ndttam.dev@gmail.com>
2019-02-21 17:36:57 +0700

    Create Point refab

---
+++ Assets/Scripts/ButtonSetup.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonSetup : MonoBehaviour {
+    public int PointerIndex;
+
+    public void HandlePressedPoint(){
+        var controller = gameObject.GetComponent<MainGameController>();
+        controller.handlePressedButton(PointerIndex);
+    }
+}
