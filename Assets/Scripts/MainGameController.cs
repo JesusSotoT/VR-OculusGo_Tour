a75645bb26f16031d48c160b1da722eae06437da
AlbertNguyen <ndttam.dev@gmail.com>
2019-02-22 14:55:08 +0700

    CleanCode-targetObejct

--- Assets/Scripts/MainGameController.cs
+++ Assets/Scripts/MainGameController.cs
@@ -6,57 +6,40 @@ public class MainGameController : MonoBehaviour
 {
     public GameObject BackgroundEnviroment;
     public GameObject VRPlayerController;
-    //Texture list
-    //public Material[] env_material_list;
-
     public Camera mainCamera;
-
     // Transforms to act as start and end markers for the journey.
     public Transform startMarker;
     private GameObject[] endMarkerList;
-
     // Movement speed in units/sec.
     public float speed;
-
     // Time when the movement started.
     private float startTime;
     private float fracJourney;
-
     // Total distance between the markers.
     private float journeyLength = 1;
-
     private int indexItemInEndList;
-
     private Vector3 cameraFirstPosition;
-
     private bool isFacingTime = false;
     private bool isCanRun = false;
     private bool isMoving = false;
     float startTimeRot = 0;
-
     private string sceneNameWillBeLoad;
     // Use this for initialization
     void Start()
     {
         cameraFirstPosition = startMarker.position;
-
         // collect object with tag 
         endMarkerList = GameObject.FindGameObjectsWithTag("EndMarker");
     }
-
-    // Update is called once per frame
     void Update()
     {
         // Rotate camera face for opposite
         if(isFacingTime){
             float distCoveredRot = (Time.time - startTimeRot) * speed;
-            Debug.Log("First function ");
             // Set vector for target rotation 
             var targetRotation = Quaternion.LookRotation(targetObject.transform.position - mainCamera.gameObject.transform.position);
-            Debug.Log("targetRotation " + targetRotation +"\n Time: "+Time.deltaTime);
             // Rotate camera to opposite with endMarker(target Marker)
             mainCamera.gameObject.transform.rotation = Quaternion.Slerp(mainCamera.gameObject.transform.rotation, targetRotation, distCoveredRot);
-            Debug.Log("transform: "+ mainCamera.gameObject.transform.rotation);
 
         }
 
@@ -64,16 +47,13 @@ public class MainGameController : MonoBehaviour
         {
             // Distance moved = time * speed.
             float distCovered = (Time.time - startTime) * speed;
-
             // Fraction of journey completed = current distance divided by total distance.
             fracJourney = distCovered / journeyLength;
-
             // Set position with custom Z
-            Vector3 endMarkerPoint = new Vector3(targetObject.gameObject.transform.position.x, targetObject.gameObject.transform.position.y, targetObject.gameObject.transform.position.z);
-
+            Vector3 targetPosition = targetObject.gameObject.transform.position;
+            Vector3 endMarkerPoint = new Vector3(targetPosition.x, targetPosition.y, targetPosition.z);
             // Set our position as a fraction of the distance between the markers.
             VRPlayerController.gameObject.transform.position = Vector3.Lerp(cameraFirstPosition, endMarkerPoint, fracJourney);
-
         }
 
         if(fracJourney >= 0.8)
@@ -89,23 +69,15 @@ public class MainGameController : MonoBehaviour
     private GameObject targetObject;
     public void  handlePressedButton(int mode, string sceneName)
     {
-        Debug.Log("Mode: " + mode + "\n Scene Name: " + sceneName);
-
         /*
          * Find GameObject with Name to get it to handle
         */
-
-       
-
         for (int i = 0; i < endMarkerList.Length; i++ ){
             string nameObject = endMarkerList[i].name;
             if (nameObject.Equals(nameFormat + mode)) {
                 targetObject = endMarkerList[i];
             }
-            // Skip if not equals target's name
         }
-
-
         if (!isMoving){
             sceneNameWillBeLoad = sceneName;
             lookAtWithEndMarker();
@@ -120,7 +92,6 @@ public class MainGameController : MonoBehaviour
         SceneManager.LoadScene(sceneNameWillBeLoad, LoadSceneMode.Single);
         // Reset object
         previousFirstCameraPosition();
-
     }
 
     private void makeZoom(int index)
@@ -131,9 +102,7 @@ public class MainGameController : MonoBehaviour
         // Calculate the journey length.
         journeyLength = Vector3.Distance(startMarker.position, targetObject.gameObject.transform.position);
         isCanRun = true;
-
         isMoving = true;
-
     }
 
     private void  lookAtWithEndMarker(){
